b772bfe33350c872b4232a5fea6cd3fe5c2c17c9
Alvin Siew <alvin_siew@tech.gov.sg>
2022-03-16 12:24:22 +0800

    Workshop01 first version

---
+++ workshop01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+type Message struct {
+   Quote string
+   Repo string
+}
+
+func main() {
+   rand.Seed(time.Now().Unix())
+   quotes := []string{
+    "Logic will get you from A to B. Imagination will take you everywhere.",
+    "There are 10 kinds of people. Those who know binary and those who don't.",
+    "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+    "It's not that I'm so smart, it's just that I stay with problems longer.",
+    "It is pitch dark. You are likely to be eaten by a grue.",
+   }
+   // n := rand.Int() % len(quotes)
+
+   templates := template.Must(template.ParseFiles("templates/index.html"))
+
+   http.Handle("/static/",
+      http.StripPrefix("/static/",
+         http.FileServer(http.Dir("static"))))
+
+   http.HandleFunc("/" , func(w http.ResponseWriter, r *http.Request) {
+      n := rand.Int() % len(quotes)
+      message := Message{quotes[n], "Repository:"}
+
+      if err := templates.ExecuteTemplate(w, "index.html", message); err != nil {
+         http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
+   })
+
+   fmt.Println("Listening");
+   fmt.Println(http.ListenAndServe(":8080", nil));
+}
